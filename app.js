a5b2b43e3cc0d4c8649adfa858a80f0b7e2030b3
denistita <denistita43@gmail.com>
2020-05-26 17:30:23 +0000

    added mongolab

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
     
-mongoose.connect("mongodb://localhost/yelp_camp_v10", {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost/yelp_camp_v10", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://denistita:johnpierre@cluster0-m50mu.mongodb.net/test?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+}).then(() => {
+	console.log('Connected to DB!');
+}).catch(err => {
+	console.log('ERROR:', err.message);
+});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
