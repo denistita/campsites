a7933e802808250248caddae472df390b7e9abfb
denistita <denistita43@gmail.com>
2020-05-26 22:44:56 +0000

    modified version

--- app.js
+++ app.js
@@ -60,6 +60,8 @@ app.use(function(req, res, next){
 app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
-app.listen(3000, function(){
-   console.log("The YelpCamp Server Has Started!");
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
 });
